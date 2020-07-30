37ba814b119b7d1de51f441abf800e6aa1968a70
Murat AKMAMMEDOV <muratx10@gmail.com>
2020-07-30 22:58:13 +0300

    refactor: add ContextAPI for state

--- task6/src/App.jsx
+++ task6/src/App.jsx
@@ -1,28 +1,79 @@
 import React from 'react';
-import { ReactComponent as Logo } from './logo.svg';
-import classes from "./App.module.scss";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Header from "./components/header/index";
-import Homepage from "./components/homepage/index";
-import Employees from "./components/employees/index";
+import classes from './App.module.scss';
+import Employees from './components/employees/index';
+import Header from './components/header/index';
+import Homepage from './components/homepage/index';
+import { Context } from './context';
+import { ReactComponent as Logo } from './logo.svg';
 
-function App() {
-  return (
+class App extends React.PureComponent {
+  state = {
+    list: [],
+    value: '',
+  };
+
+  componentDidMount() {
+    if (this.state.list.length === 0) {
+      fetch('https://reqres.in/api/users?per_page=12')
+        .then(res => res.json())
+        .then(list => this.setState({list: list.data}));
+    }
+  }
+
+  onDelete = (id) => {
+    this.setState(state => ({
+      list: state.list.filter(item => {
+        return item.id !== id;
+      }),
+    }))
+  }
+
+  onChange = (e) => {
+    e.persist();
+    this.setState(state => ({
+      value: e.target.value,
+    }))
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.setState(state => ({
+      list: [...state.list, {
+        first_name: state.value,
+        id: new Date().getMilliseconds(),
+      }],
+      value: '',
+    }))
+  }
+
+  render() {
+    return (
       <div>
         <BrowserRouter>
-            <div className={classes.header}>
-                <div className={classes.logo}>
-                    <Logo />
-                </div>
-                <Header />
+          <div className={classes.header}>
+            <div className={classes.logo}>
+              <Logo />
             </div>
-            <Switch>
-                <Route exact path="/" component={Homepage}/>
-                <Route path="/employees" component={Employees} />
-            </Switch>
+            <Header />
+          </div>
+          <Switch>
+            <Context.Provider
+              value={{
+                state: this.state,
+                onDelete: this.onDelete,
+                onChange: this.onChange,
+                onSubmit: this.onSubmit,
+              }}
+            >
+              <Route exact path="/" component={Homepage} />
+              <Route path="/employees" component={Employees} />
+            </Context.Provider>
+          </Switch>
         </BrowserRouter>
       </div>
-  )
+    )
+  }
 }
 
 export default App;
--- task6/src/components/employees/employees.component.jsx
+++ task6/src/components/employees/employees.component.jsx
@@ -1,64 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Context } from '../../context';
 import Person from '../person';
 import UserAddInput from '../userAddInput';
 import classes from './employees.module.scss';
 
-class Employees extends React.PureComponent {
-  state = {
-    list: [],
-    value: '',
-  }
-
-  componentDidMount() {
-    if(this.state.list.length === 0)
-      fetch('https://reqres.in/api/users?per_page=12')
-      .then(res => res.json())
-      .then(list => this.setState({list: list.data}));
-  }
-
-  delete = (id) => {
-    this.setState(state => ({
-      list: state.list.filter(item => {
-        return item.id !== id;
-      }),
-    }))
-  }
-
-  onChange = (e) => {
-    e.persist();
-    this.setState(state => ({
-      value: e.target.value
-    }))
-  }
-
-  onSubmit = () => {
-    this.setState(state => ({
-     list: [...state.list, {
-       first_name: state.value,
-       id: new Date().getMilliseconds()
-     }],
-      value: ''
-    }))
-  }
-
-
-  render() {
-    const {list} = this.state;
-    return (
-      <>
-        <UserAddInput
-          value={this.state.value}
-          change={this.onChange}
-          submit={this.onSubmit}
-        />
-        <div className={classes.employees}>
-          {list.map(user => (
-            <Person key={user.id} user={user} deleteItem={this.delete} />
-          ))}
-        </div>
-      </>
-    )
-  }
+const Employees = () => {
+  const context = useContext(Context);
+  return (
+    <>
+      <UserAddInput
+        value={context.state.value}
+        change={context.onChange}
+        submit={context.onSubmit}
+      />
+      <div className={classes.employees}>
+        {context.state.list.map(user => (
+          <Person key={user.id} user={user} deleteItem={context.onDelete} />
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default Employees;
